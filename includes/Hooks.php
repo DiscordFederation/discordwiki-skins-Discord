e6dac98c0d9368f37091f3cbdb5f9e8f670ae039
Vivek Joshy <daegontaven@gmail.com>
2018-12-30 17:21:48 -0500

    Add hook to override notification icons
    The relative path is not really an issue for the hook being invoked here. Just make sure the paths don't have typos.
    
    Sources:
     - https://discourse-mediawiki.wmflabs.org/t/how-do-i-configure-extensions-from-a-skin/1009?u=daegontaven
     - https://www.mediawiki.org/wiki/Notifications/Developer_guide#Icons
     - https://github.com/wikimedia/mediawiki-extensions-Echo/blob/4c14320a32d1d8f33c8db1e270093c90430d7fd6/extension.json#L602-L658

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -28,4 +28,39 @@ class Hooks {
 			$sk->enableResponsiveMode();
 		}
 	}
+
+    public static function onBeforeCreateEchoEvent( &$notifications, &$notificationCategories, &$icons ) {
+        // You can use either a path or a url, but not both.
+        // The value of 'path' is relative to $wgExtensionAssetsPath.
+        //
+        // The value of 'url' should be a URL.
+
+        //replacing with custom icons
+        $echo_images = "../skins/Discord/images/ext/echo";
+        $new_icons = array(
+            "placeholder" => array("path" => "{$echo_images}/notice.svg"),
+            "chat" => array(
+                "path" => array(
+                    "ltr" => "{$echo_images}/speechBubbles-ltr-progressive.svg",
+                    "rtl" => "{$echo_images}/speechBubbles-rtl-progressive.svg"
+                    )
+            ),
+            "edit" => array("path" => "{$echo_images}/edit-progressive.svg"),
+            "edit-user-talk" => array("path" => "{$echo_images}/edit-user-talk-progressive.svg"),
+            "linked" => array("path" => "{$echo_images}/link-progressive.svg"),
+            "mention" => array("path" => "{$echo_images}/mention-progressive.svg"),
+            "mention-failure" => array("path" => "{$echo_images}/mention-failure.svg"),
+            "mention-success" => array("path" => "{$echo_images}/mention-success-constructive.svg"),
+            "mention-status-bundle" => array("path" => "{$echo_images}/mention-status-bundle-progressive.svg"),
+            "reviewed" => array("path" => "{$echo_images}/articleCheck-progressive.svg"),
+            "revert" => array("path" => "{$echo_images}/revert.svg"),
+            "user-rights" => array("path" => "{$echo_images}/user-rights-progressive.svg"),
+            "emailuser" => array("path" => "{$echo_images}/message-constructive.svg"),
+            "help" => array("path" => "{$echo_images}/help.svg"),
+            "global" => array("path" => "{$echo_images}/global-progressive.svg"),
+            "article-reminder" => array("path" => "{$echo_images}/global-progressive.svg"),
+        );
+        $icons = array_replace($icons, $new_icons);
+        return true;
+    }
 }
