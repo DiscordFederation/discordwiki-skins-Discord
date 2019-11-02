36849e3a93c587c284d422a31d9af7d7ab72c8ba
Taven Argus <daegontaven@gmail.com>
2019-11-02 13:42:41 +0530

    Update ooui theme to the 1.34 version
    This also updates Recent Changes Filter's UI

--- resources/ooui/oojs-ui-discord.js
+++ resources/ooui/oojs-ui-discord.js
@@ -1,12 +1,12 @@
 /*!
- * OOUI v0.30.0
+ * OOUI v0.35.1
  * https://www.mediawiki.org/wiki/OOUI
  *
  * Copyright 2011–2019 OOUI Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2019-01-12T09:21:32Z
+ * Date: 2019-11-02T07:38:00Z
  */
 ( function ( OO ) {
 
@@ -18,9 +18,9 @@
  *
  * @constructor
  */
-OO.ui.DiscordTheme = function OoUiDiscordTheme() {
-    // Parent constructor
-    OO.ui.DiscordTheme.parent.call( this );
+OO.ui.DiscordTheme = function OoUiWikimediaUITheme() {
+	// Parent constructor
+	OO.ui.DiscordTheme.parent.call( this );
 };
 
 /* Setup */
@@ -33,73 +33,85 @@ OO.inheritClass( OO.ui.DiscordTheme, OO.ui.Theme );
  * @inheritdoc
  */
 OO.ui.DiscordTheme.prototype.getElementClasses = function ( element ) {
-    // Parent method
-    var variant, isFramed, isActive, isToolOrGroup,
-        variants = {
-            warning: false,
-            invert: false,
-            progressive: false,
-            destructive: false
-        },
-        // Parent method
-        classes = OO.ui.DiscordTheme.parent.prototype.getElementClasses.call( this, element );
+	// Parent method
+	var variant, isFramed, isActive, isToolOrGroup,
+		variants = {
+			invert: false,
+			progressive: false,
+			destructive: false,
+			error: false,
+			warning: false,
+			success: false
+		},
+		// Parent method
+		classes = OO.ui.DiscordTheme.parent.prototype.getElementClasses.call( this, element );
 
-    if (
-        element instanceof OO.ui.IconWidget &&
-        element.$element.hasClass( 'oo-ui-checkboxInputWidget-checkIcon' )
-    ) {
-        // Icon on CheckboxInputWidget
-        variants.invert = true;
-    } else if ( element.supports( [ 'hasFlag' ] ) ) {
-        isFramed = element.supports( [ 'isFramed' ] ) && element.isFramed();
-        isActive = element.supports( [ 'isActive' ] ) && element.isActive();
-        isToolOrGroup =
-            // Check if the class exists, as classes that are not in the 'core' module may not be loaded
-            ( OO.ui.Tool && element instanceof OO.ui.Tool ) ||
-            ( OO.ui.ToolGroup && element instanceof OO.ui.ToolGroup );
-        if (
-            // Button with a dark background
-            isFramed && ( isActive || element.isDisabled() || element.hasFlag( 'primary' ) ) ||
-            // Toolbar with a dark background
-            isToolOrGroup && element.hasFlag( 'primary' )
-        ) {
-            // … use white icon / indicator, regardless of other flags
-            variants.invert = true;
-        } else if ( !isFramed && element.isDisabled() ) {
-            // Frameless disabled button, always use black icon / indicator regardless of other flags
-            variants.invert = false;
-        } else if ( !element.isDisabled() ) {
-            // Any other kind of button, use the right colored icon / indicator if available
-            variants.progressive = element.hasFlag( 'progressive' ) ||
-                // Active tools/toolgroups
-                ( isToolOrGroup && isActive ) ||
-                // Pressed or selected outline/menu option widgets
-                (
-                    (
-                        element instanceof OO.ui.MenuOptionWidget ||
-                        // Check if the class exists, as classes that are not in the 'core' module may not be loaded
-                        ( OO.ui.OutlineOptionWidget && element instanceof OO.ui.OutlineOptionWidget )
-                    ) &&
-                    ( element.isPressed() || element.isSelected() )
-                );
+	if (
+		element instanceof OO.ui.IconWidget &&
+		// eslint-disable-next-line no-jquery/no-class-state
+		element.$element.hasClass( 'oo-ui-checkboxInputWidget-checkIcon' )
+	) {
+		// Icon on CheckboxInputWidget
+		variants.invert = true;
+	} else if ( element.supports( [ 'hasFlag' ] ) ) {
+		isFramed = element.supports( [ 'isFramed' ] ) && element.isFramed();
+		isActive = element.supports( [ 'isActive' ] ) && element.isActive();
+		isToolOrGroup =
+			// Check if the class exists, as classes that are not in the 'core' module may
+			// not be loaded.
+			( OO.ui.Tool && element instanceof OO.ui.Tool ) ||
+			( OO.ui.ToolGroup && element instanceof OO.ui.ToolGroup );
+		if (
+			// Button with a dark background.
+			isFramed && ( isActive || element.isDisabled() || element.hasFlag( 'primary' ) ) ||
+			// Toolbar with a dark background.
+			isToolOrGroup && element.hasFlag( 'primary' )
+		) {
+			// … use white icon / indicator, regardless of other flags
+			variants.invert = true;
+		} else if ( !isFramed && element.isDisabled() && !element.hasFlag( 'invert' ) ) {
+			// Frameless disabled button, always use black icon / indicator regardless of
+			// other flags.
+			variants.invert = false;
+		} else if ( !element.isDisabled() ) {
+			// Any other kind of button, use the right colored icon / indicator if available.
+			variants.progressive = element.hasFlag( 'progressive' ) ||
+				// Active tools/toolgroups
+				( isToolOrGroup && isActive ) ||
+				// Pressed or selected outline/menu option widgets
+				(
+					(
+						element instanceof OO.ui.MenuOptionWidget ||
+						// Check if the class exists, as classes that are not in the 'core' module
+						// may not be loaded.
+						(
+							OO.ui.OutlineOptionWidget &&
+							element instanceof OO.ui.OutlineOptionWidget
+						)
+					) &&
+					( element.isPressed() || element.isSelected() )
+				);
 
-            variants.destructive = element.hasFlag( 'destructive' );
-            variants.warning = element.hasFlag( 'warning' );
-        }
-    }
+			variants.destructive = element.hasFlag( 'destructive' );
+			variants.invert = element.hasFlag( 'invert' );
+			variants.error = element.hasFlag( 'error' );
+			variants.warning = element.hasFlag( 'warning' );
+			variants.success = element.hasFlag( 'success' );
+		}
+	}
 
-    for ( variant in variants ) {
-        classes[ variants[ variant ] ? 'on' : 'off' ].push( 'oo-ui-image-' + variant );
-    }
+	for ( variant in variants ) {
+		classes[ variants[ variant ] ? 'on' : 'off' ].push( 'oo-ui-image-' + variant );
+	}
 
-    return classes;
+	return classes;
 };
 
 /**
  * @inheritdoc
  */
 OO.ui.DiscordTheme.prototype.getDialogTransitionDuration = function () {
-    return 250;
+	return 250;
 };
 
 /* Instantiation */
